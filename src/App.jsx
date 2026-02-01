d019a5c34cbf090f04b2517b18403ba0bfaf9118
Alireza <aliareya2003@gmail.com>
2026-02-01 21:39:38 +0430

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
   console.log(import.meta.env.VITE_API_URL)
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Tailwind v3 is working!
-    </h1>
+    <MainLayout/>
   );
 }
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainLayout() {
+  return (
+    <div className='w-full '>
+      <div className='w-full h-20 bg-slate-300 '>aaaaa</div>
+
+    </div>
+  )
+}
+
+export default MainLayout
