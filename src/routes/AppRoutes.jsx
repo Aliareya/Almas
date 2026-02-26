5c0a629d487ad61aea9c23d4827bc88bcb8564ec
Alireza <aliareya2003@gmail.com>
2026-02-26 13:14:59 +0430

    ff

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,7 +3,7 @@ import Home from '../pages/home/Home';
 import Login from '../pages/auth/Login';
 import Register from '../pages/auth/Register';
 import Shop from '../pages/shop/Shop';
-import ProductDetails from '../pages/products_details/ProductDetails';
+import ProductDetails from '../pages/productdetails/ProductDetails';
 
 function AppRoutes() {
   return (
